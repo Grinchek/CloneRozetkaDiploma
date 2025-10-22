2fad29045554840f44274f08536f6a4aa73d24f2
Grinchek <warning686@gmail.com>
2025-10-22 17:44:20 +0300

    The sceleton of project was added

--- CloneRozetkaApi/CloneRozetkaApi/Program.cs
+++ CloneRozetkaApi/CloneRozetkaApi/Program.cs
@@ -1,15 +1,52 @@
+﻿using Core.Interfaces;
+using Core.Services;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using CloneRozetkaApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("MyAtbConnection")));
+
+builder.Services.AddScoped<IImageService, ImageService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
+
+// ������ �������
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors(u =>
+    u.AllowAnyHeader()
+        .AllowAnyOrigin()
+        .AllowAnyMethod());
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+var dir = builder.Configuration["ImagesDir"];
+string path = Path.Combine(Directory.GetCurrentDirectory(), dir);
+Directory.CreateDirectory(path);
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(path),
+    RequestPath = $"/{dir}"
+});
+
+await app.SeedData();
 
 app.Run();
