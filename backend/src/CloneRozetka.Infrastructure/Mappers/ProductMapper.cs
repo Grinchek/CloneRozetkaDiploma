0ef9ba1952719d35415c862e9d28dd9c400e8f25
novakvova <novakvova@gmail.com>
2025-11-21 16:02:15 +0200

    Add AutoMapper is good

---
+++ backend/src/CloneRozetka.Infrastructure/Mappers/ProductMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CloneRozetka.Application.Products.DTOs;
+using CloneRozetka.Domain.Entities;
+
+namespace CloneRozetka.Infrastructure.Mappers;
+
+public class ProductMapper : Profile
+{
+    public ProductMapper()
+    {
+        CreateMap<ProductEntity, ProductListItemDto>()
+            .ForMember(dest => dest.MainImageUrl, opt => opt
+                .MapFrom(x => x.ProductImages
+                    .OrderBy(pi => pi.Priority)
+                    .Select(x =>$"/images/400_{x.Name}").FirstOrDefault()));
+        ;
+    }
+}
